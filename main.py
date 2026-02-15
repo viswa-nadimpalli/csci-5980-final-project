b46f07de7673ad85a0066fa70396b39956af3c49
Viswa Nadimpalli <viswanadim@gmail.com>
2026-02-15 13:56:15 -0600

    initial iteration with per-key locks + updated toml file with dependencies

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ logger.setLevel(logging.INFO)
 
 _file = logging.FileHandler("kv_store.log")
 _formatter = logging.Formatter(
-    format='%(asctime)s %(levelname)s %(message)s',
+    fmt='%(asctime)s %(levelname)s %(message)s',
     datefmt='%Y-%m-%d %H:%M:%S',
 )
 
@@ -30,6 +30,7 @@ kv_store = {} # {key (str): value (str)}
 
 # LOCKS
 key_locks = {} # {key (str): lock (asyncio.Lock)}
+
 key_locks_lock = asyncio.Lock()
 
 save_task = asyncio.Task
@@ -42,18 +43,12 @@ async def get_lock_for_key(key):
             key_locks[key] = lock
         return lock
 
-def save_snapshot_to_disk(snapshot):
+def save_to_disk(snapshot):
     tmp = DATA_FILE + ".tmp"
     with open(tmp, "w") as f:
         json.dump(snapshot, f)
     os.replace(tmp, DATA_FILE)
-
-# save to disk
-async def save_to_disk():
-    try:
-        snapshot = dict(kv_store)
-        save_snapshot_to_disk(snapshot)
-        logger.info(f"Saved snapshot")
+    logger.info(f"Saved {len(snapshot)} key-value pairs to disk.")
 
 # load from disk
 def load_from_disk():
@@ -72,15 +67,44 @@ async def periodic_save():
         await asyncio.sleep(INTERVAL_SAVE_SECONDS)
 
         async with key_locks_lock:
-            snapshot = dict(kv_store)
+            locks = list(key_locks.values())
         
-        await asyncio.to_thread(save_snapshot_to_disk, snapshot)
-            
+        for lock in locks:
+            await lock.acquire()
+
+        snapshot = dict(kv_store)
+
+        for lock in locks:
+            lock.release()
+        
+        await asyncio.to_thread(save_to_disk, snapshot)
+        logger.info(f"Saved key-value to disk.")
+
 def cleanup():
     logger.info("EXIT")
     save_to_disk()
 
-# API ENDPOINTS
+# ---- STARTUP AND SHUTDOWN ----
+@app.on_event("startup")
+async def on_startup():
+    global save_task
+    load_from_disk()
+    save_task = asyncio.create_task(periodic_save())
+    logger.info("Server started")
+
+@app.on_event("shutdown")
+async def on_shutdown():
+    global save_task
+
+    if save_task is not None:
+        save_task.cancel()
+        try:
+            await save_task
+        except asyncio.CancelledError:
+            pass
+
+
+# ---- API ENDPOINTS ----
 @app.post("/{key}")
 async def put_key_value(key: str, value: str):
     # concurrency / locking
@@ -107,8 +131,6 @@ async def delete_key_value(key: str):
         del kv_store[key]
     logger.info(f"DELETE key='{key}'")
     return {"message": f"Key '{key}' deleted successfully."}
-
-atexit.register(cleanup)
 
 if __name__ == "__main__":
     uvicorn.run(app, host="127.0.0.1", port=8080)
